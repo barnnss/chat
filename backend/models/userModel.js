835bf7854f013ecbd9a6d12bc5bbc20725d95d74
Barnns <s.barnnita@gmail.com>
2023-07-03 21:46:32 +0545

    c

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+//const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema(
   {
@@ -21,7 +21,7 @@ const userSchema = mongoose.Schema(
   { timestaps: true }
 );
 
-userSchema.methods.matchPassword = async function (enteredPassword) {
+/*userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
@@ -32,8 +32,8 @@ userSchema.pre("save", async function (next) {
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-});
+}); */
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema); 
 
-module.exports = User;
+module.exports = User; 
--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,20 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
-
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 
 connectDB(); 
 const app = express();
 
+app.use(express.json()); //to accept JSON data
+
 app.get('/', (req, res) => {
     res.send("Api is running succesfully");
-})
-
-app.get("/api/chat", (req, res) => {
-    res.send(chats);
 });
+
+app.use('/api/user', userRoutes);
 
 const PORT = process.env.PORT || 5000 
 
